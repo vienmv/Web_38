af93c72acad5c078b58fde665ba501c6d12dd16e
vienmv <vienmv.dev@gmail.com>
2020-06-15 20:59:14 +0700

    lib build-in

--- session_4/main.js
+++ session_4/main.js
@@ -1,12 +1,12 @@
 //learning
 //1. require() other file 
-const {str : myString,str2} =require('./library')
-console.log(str2)
-console.log(myString)
-const { number} =require('./library2')
-console.log(number)
+// const {str : myString,str2} =require('./library')
+// console.log(str2)
+// console.log(myString)
+// const { number} =require('./library2')
+// console.log(number)
 //2.require() build-in package 
-// const fs=require('fs')
+//const fs=require('fs')
 // const filePath='./package.json'
 // const fileContent=fs.readFileSync(filePath,'utf-8')
 // console.log(fileContent)
@@ -29,3 +29,38 @@ console.log(number)
 //xoa file fs.unlink
 //xoa thu muc fs.mrdir
 //3. require() installed package
+
+
+// const os = require("os") 
+// let platform = os.platform()
+// console.log(platform) 
+
+
+// const path =require('path')
+// console.log(__dirname)
+// console.log(path.join(__dirname),'package.json')
+// console.log(path.resolve(__dirname),'../package.json')
+
+
+
+// hash MD5
+// const crypto = require('crypto') // encode/decode, hash
+// let str ="abc"
+// let strHashed= hashMd5(str)
+// console.log('str hash',strHashed)
+
+// function hashMd5(str){
+//     return crypto.createHash('md5').update(str).digest("hex")
+// }
+
+
+
+
+
+// const zlib= require("zlib")
+// let strSource="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc elementum hendrerit suscipit. Donec vel mattis nulla, ut ultricies ante. Sed pellentesque dui non hendrerit bibendum. Cras bibendum imperdiet augue, nec ullamcorper purus lacinia posuere. Suspendisse gravida ornare justo, eu ultrices urna rhoncus iaculis. In ullamcorper egestas erat eu gravida. Aliquam mollis pretium lectus quis efficitur. Proin luctus consectetur suscipit. Suspendisse egestas suscipit sapien, sed consectetur leo mattis non. Donec semper blandit dui non rhoncus. Duis finibus aliquet lacus id bibendum."
+// let strZip = zlib.deflateSync(strSource).toString("base64")
+// console.log("Str zip " ,strZip)
+
+const util= require("util")
+
